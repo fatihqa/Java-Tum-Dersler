f99124e341e231aa6aa38e3e58106252ab22c05d
fatih <gtm3497@gmail.com>
2022-11-28 20:02:41 +0300

    java tum dersler

---
+++ src/ders46_maps/C03_SoyisimleriBuyukHarfYap.java
@@ -0,0 +1,12 @@
+package ders46_maps;
+
+public class C03_SoyisimleriBuyukHarfYap {
+    public static void main(String[] args) {
+        // ogrencimapindeki soyisilmleri buyuk harfe cevirip
+        //map i bize donduren bir method olusturalim
+
+        
+
+
+    }
+}
